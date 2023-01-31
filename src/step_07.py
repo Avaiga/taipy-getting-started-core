e1f652ed11fa5449a060dabade5fca3f811007ef
FlorianJacta <98709993+FlorianJacta@users.noreply.github.com>
2023-01-31 15:11:19 +0100

    Update step_07.py

--- src/step_07.py
+++ src/step_07.py
@@ -16,20 +16,20 @@ def add(nb):
 Config.configure_job_executions(mode="standalone", max_nb_of_workers=2)
 
 # Configuration of Data Nodes
-input_data_node_cfg = Config.configure_data_node("input", default_data=21)
-intermediate_data_node_cfg = Config.configure_data_node("intermediate", default_data=21)
-output_data_node_cfg = Config.configure_data_node("output")
+input_cfg = Config.configure_data_node("input", default_data=21)
+intermediate_cfg = Config.configure_data_node("intermediate", default_data=21)
+output_cfg = Config.configure_data_node("output")
 
 # Configuration of tasks
 first_task_cfg = Config.configure_task("double",
                                     double,
-                                    input_data_node_cfg,
-                                    intermediate_data_node_cfg)
+                                    input_cfg,
+                                    intermediate_cfg)
 
 second_task_cfg = Config.configure_task("add",
                                     add,
-                                    intermediate_data_node_cfg,
-                                    output_data_node_cfg)
+                                    intermediate_cfg,
+                                    output_cfg)
 
 # Configuration of the pipeline and scenario
 pipeline_cfg = Config.configure_pipeline("my_pipeline", [first_task_cfg, second_task_cfg])
